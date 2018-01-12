181cd878973213606e4851b656b898b8bc8b2ec5
Saurabh Mithe <saurabhmithe@gmail.com>
2018-01-12 11:25:45 -0800

    adding solution to StringCompression

--- ArraysAndStrings/StringCompression.java
+++ ArraysAndStrings/StringCompression.java
@@ -1,44 +1,54 @@
-
 /*
- * Implement a method to perform basic string compression using 
- * the counts of repeated characters. For example, the string 
- * aabcccccaaa would become a2b1c5a3. If the "compressed" string 
- * would not become smaller than the original string, your method 
- * should return the original string. You can assume the string has 
+ * Implement a method to perform basic string compression using
+ * the counts of repeated characters. For example, the string
+ * aabcccccaaa would become a2b1c5a3. If the "compressed" string
+ * would not become smaller than the original string, your method
+ * should return the original string. You can assume the string has
  * only uppercase and lowercase letters (a - z).
  */
 public class StringCompression {
-	public static void main(String[] args) {
-		String toBeCompressed = "aaabbbbbccccccc";
+    public static void main(String[] args) {
+        String toBeCompressed = "aaabbbbbccccccc";
+        System.out.println(compressString(toBeCompressed));
+    }
 
-		System.out.println(compressString(toBeCompressed));
-	}
+    /**
+     * This approach looks at each character starting from the beginning of the string.
+     * If the previous character is the same as the current character, count is incremented and maintained.
+     * When a new character is encountered, the previous character along with its count is appended to
+     * the compressed string and a new count is maintained for the new character.
+     *
+     * The time complexity for this solution is O(N).
+     *
+     * @param s
+     * @return
+     */
+    private static String compressString(String s) {
+        StringBuilder compressedString = new StringBuilder();
 
-	private static String compressString(String s) {
+        int count = 0;
+        char previous = ' ';
+        for (int i = 0; i < s.length(); i++) {
+            if (previous != ' ') {
+                if (s.charAt(i) == previous) {
+                    // This character is the same as the previous one
+                    count++;
+                } else {
+                    // This is a new character. Appending the count of previous distinct character to compressed string
+                    compressedString.append(previous);
+                    compressedString.append(count);
+                    count = 1;
+                }
+            } else {
+                // This is the first character in the string
+                count++;
+            }
+            previous = s.charAt(i);
 
-		StringBuilder compressedString = new StringBuilder();
+        }
+        compressedString.append(previous);
+        compressedString.append(count);
 
-		int count = 0;
-		char previous = ' ';
-		for (int i = 0; i < s.length(); i++) {
-			if (previous != ' ') {
-				if (s.charAt(i) == previous) {
-					count++;
-				} else {
-					compressedString.append(previous);
-					compressedString.append(count);
-					count = 1; // This is a new character. Acknowledging it's existence.
-				}
-			} else {
-				// This just started. Setting count 1 for first letter
-				count++;
-			}
-			previous = s.charAt(i);
-
-		}
-		compressedString.append(previous);
-		compressedString.append(count);
-
-		return compressedString.toString();
-	}
+        return compressedString.toString();
+    }
 }
