055259ab88d8112c6184b571ced70cd941e1dfbf
Saurabh Mithe <saurabhmithe@gmail.com>
2018-06-25 02:28:53 -0700

    modified approach

--- Solutions/src/ArraysAndStrings/ZeroMatrix.java
+++ Solutions/src/ArraysAndStrings/ZeroMatrix.java
@@ -1,94 +1,83 @@
+package ArraysAndStrings;
 
-/*
- * Write an algorithm such that if an element in an MxN matrix is 0, 
- * its entire row and column are set to O.
- */
 public class ZeroMatrix {
-	public static void main(String[] args) {
-		int m = 4;
-		int n = 5;
-		int[][] matrix = new int[m][n];
 
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 5; j++) {
-				matrix[i][j] = i + j + 2;
-			}
-		}
-		matrix[0][3] = 0;
-		matrix[1][2] = 0;
-		matrix[2][3] = 0;
-		printMatrix(nullifyZeros(matrix, m, n));
-	}
+	/*
 
-	/**
-	 * In this approach, we use two boolean arrays to maintain the row number to be nullified and the
-	 * column number to be nullified. We access each element in the given matrix and if the element is 0,
-	 * we add the set the value corresponding to the row number in the row boolean array to true.
-	 * Similarly, we set the value corresponding to the column number in the column boolean array to true.
-	 *
-	 * Now that we have two arrays which indicate the row number and column number to be nullified respectively,
-	 * we traverse through the arrays and set the row and column to 0 in the original matrix according
-	 * to the data in the arrays.
-	 *
-	 * Since we need to access all the elements in the matrix, the time complexity for this solution would be O(N^2).
-	 *
-	 * @param matrix
-	 * @param m
-	 * @param n
-	 * @return
-	 */
-	private static int[][] nullifyZeros(int[][] matrix, int m, int n) {
+	Write an algorithm such that if an element in an MxN matrix is 0,
+	its entire row and column are set to O.
 
-		printMatrix(matrix);
+	*/
 
-		boolean[] toNullifyRow = new boolean[m];
-		boolean[] toNullifyColumn = new boolean[n];
+    public static void main(String[] args) {
+        int m = 4;
+        int n = 5;
+        int[][] matrix = new int[m][n];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                matrix[i][j] = i + j + 2;
+            }
+        }
+        matrix[0][3] = 0;
+        matrix[1][2] = 0;
+        matrix[2][3] = 0;
+        nullifyZeros(matrix, m, n);
 
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				if (matrix[i][j] == 0) {
-					toNullifyRow[i] = true;
-					toNullifyColumn[j] = true;
-				}
-			}
-		}
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("  " + matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
 
-		for (int i = 0; i < toNullifyRow.length; i++) {
-			if (toNullifyRow[i]) {
-				nullifyRow(i, matrix);
-			}
-		}
+    /**
+     * In this approach, we use two boolean arrays to maintain the row number to be nullified and the
+     * column number to be nullified. We access each element in the given matrix and if the element is 0,
+     * we add the set the value corresponding to the row number in the row boolean array to true.
+     * Similarly, we set the value corresponding to the column number in the column boolean array to true.
+     * <p>
+     * Now that we have two arrays which indicate the row number and column number to be nullified respectively,
+     * we traverse through the arrays and set the row and column to 0 in the original matrix according
+     * to the data in the arrays.
+     * <p>
+     * Time Complexity - O(m * n)
+     * Space Complexity - O(m + n)
+     */
+    private static int[][] nullifyZeros(int[][] matrix, int m, int n) {
+        boolean[] toNullifyRow = new boolean[m];
+        boolean[] toNullifyColumn = new boolean[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    toNullifyRow[i] = true;
+                    toNullifyColumn[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < toNullifyRow.length; i++) {
+            if (toNullifyRow[i]) {
+                nullifyRow(i, matrix);
+            }
+        }
+        for (int i = 0; i < toNullifyColumn.length; i++) {
+            if (toNullifyColumn[i]) {
+                nullifyColumn(i, matrix);
+            }
+        }
+        return matrix;
+    }
 
-		printMatrix(matrix);
+    private static void nullifyRow(int i, int[][] matrix) {
+        for (int j = 0; j < matrix[0].length; j++) {
+            matrix[i][j] = 0;
+        }
+    }
 
-		for (int i = 0; i < toNullifyColumn.length; i++) {
-			if (toNullifyColumn[i]) {
-				nullifyColumn(i, matrix);
-			}
-		}
+    private static void nullifyColumn(int i, int[][] matrix) {
+        for (int j = 0; j < matrix.length; j++) {
+            matrix[j][i] = 0;
+        }
+    }
 
-		return matrix;
-	}
-
-	private static void nullifyRow(int i, int[][] matrix) {
-		for (int j = 0; j < matrix[0].length; j++) {
-			matrix[i][j] = 0;
-		}
-	}
-
-	private static void nullifyColumn(int i, int[][] matrix) {
-		for (int j = 0; j < matrix.length; j++) {
-			matrix[j][i] = 0;
-		}
-	}
-
-	private static void printMatrix(int[][] matrix) {
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 5; j++) {
-				System.out.print("  " + matrix[i][j]);
-			}
-			System.out.println();
-		}
-		System.out.println();
-	}
 }
