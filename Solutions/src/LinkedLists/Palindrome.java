60d3e07289f804006bc46766ea9ef3a8a138f1b3
Saurabh Mithe <saurabhmithe@gmail.com>
2018-06-29 18:13:36 -0700

    added LinkedList problem

---
+++ Solutions/src/LinkedLists/Palindrome.java
@@ -0,0 +1,51 @@
+package LinkedLists;
+
+public class Palindrome {
+
+    public boolean palindrome(LinkedList.Node head) {
+        LinkedList.Node fast = head;
+        LinkedList.Node slow = head;
+        LinkedList.Node end = head;
+        while (fast.next != null) {
+            fast = fast.next.next;
+            end = slow;
+            slow = slow.next;
+        }
+        if (fast != null) {
+            end = slow;
+            slow = slow.next;
+        }
+        LinkedList.Node prev = null;
+        LinkedList.Node next;
+        LinkedList.Node current = slow;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        end.next = prev;
+        LinkedList.Node runner = head;
+        while (prev != null) {
+            if (prev.data != runner.data) return false;
+            runner = runner.next;
+            prev = prev.next;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.append(1);
+        list.append(2);
+        list.append(3);
+        list.append(4);
+        list.append(3);
+        list.append(2);
+        list.append(1);
+
+        Palindrome palindrome = new Palindrome();
+        boolean isPalindrome = palindrome.palindrome(list.getHead());
+        System.out.println(isPalindrome);
+    }
+}
--- Solutions/src/LinkedLists/SumLists.java
+++ Solutions/src/LinkedLists/SumLists.java
@@ -2,6 +2,25 @@ package LinkedLists;
 
 public class SumLists {
 
+    /*
+
+    You have two numbers represented by a linked list, where each node contains a single digit.
+    The digits are stored in reverse order, such that the 1's digit is at the head of the list.
+    Write a function that adds the two numbers and returns the sum as a linked list.
+
+    EXAMPLE
+    Input: (7-) 1 -) 6) + (5 -) 9 -) 2).Thatis,617 + 295. Output: 2 -) 1 -) 9.That is, 912.
+
+    FOLLOW UP
+    Suppose the digits are stored in forward order. Repeat the above problem. EXAMPLE
+    Input: (6 -) 1 -) 7) + (2 -) 9 -) 5).Thatis,617 + 295. Output: 9 -) 1 -) 2.That is, 912.
+
+    */
+
+    /**
+     * Time Complexity - O(n)
+     * Space Complexity - O(n)
+     */
     public LinkedList sumLists(LinkedList.Node head1, LinkedList.Node head2) {
         LinkedList sumList = new LinkedList();
         LinkedList.Node runner1 = head1;
@@ -64,4 +83,5 @@ public class SumLists {
             node = node.next;
         }
     }
+
 }
