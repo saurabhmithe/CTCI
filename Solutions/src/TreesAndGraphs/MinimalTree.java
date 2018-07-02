1288ca3f513e6630c0e507357300c91bcb652c26
Saurabh Mithe <saurabhmithe@gmail.com>
2018-07-02 01:49:26 -0700

    added Trees&Graphs problem

---
+++ Solutions/src/TreesAndGraphs/MinimalTree.java
@@ -0,0 +1,29 @@
+package TreesAndGraphs;
+
+public class MinimalTree {
+
+    public TreeNode constructTree(int[] a, int low, int high) {
+        if (low > high) return null;
+        int mid = (high + low) / 2;
+        TreeNode newNode = new TreeNode(a[mid]);
+        newNode.left = constructTree(a, low, mid - 1);
+        newNode.right = constructTree(a, mid + 1, high);
+
+        return newNode;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        MinimalTree minimalTree = new MinimalTree();
+        TreeNode root = minimalTree.constructTree(a, 0, a.length - 1);
+        minimalTree.print(root);
+    }
+
+    public void print(TreeNode root) {
+        if (root != null) {
+            print(root.left);
+            System.out.println(root.value);
+            print(root.right);
+        }
+    }
+}
---
+++ Solutions/src/TreesAndGraphs/TreeNode.java
@@ -0,0 +1,11 @@
+package TreesAndGraphs;
+
+public class TreeNode {
+    int value;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int value) {
+        this.value = value;
+    }
+}
